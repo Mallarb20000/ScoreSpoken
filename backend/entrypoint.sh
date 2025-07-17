ffd39b62bbf8b1a39dd623a0c43818e2f2f439e6
Rohit Malla <P2779735@my365.dmu.ac.uk>
2025-07-17 16:32:05 +0100

    fix

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,7 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+
 # Start the Gunicorn server (or whatever you use)
 echo "Starting Gunicorn server..."
 poetry run gunicorn score.wsgi:application --bind 0.0.0.0:8000
